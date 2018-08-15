2494a5cd27801de84ed55cd4150fb2f4a4efb1d9
lihuaiqiang <314207558@qq.com>
2018-08-16 00:59:14 +0800

    add secondWebProject

---
+++ src/com/aixuexi/Test1.java
@@ -0,0 +1,10 @@
+package com.aixuexi;
+
+import java.io.FileOutputStream;
+
+public class Test1 {
+    public void main(String[] args){
+        String[] array = {"a","b"};
+
+    }
+}
---
+++ web/JQueryTest/Demo/Demo02.选择器/template-web.js
@@ -0,0 +1,6 @@
+var jQuery = function( selector, context ) {
+<script type="xxxx" id="tpl">
+    <p>我叫<span><%= name %></span>，今年<span><%= age %></span>岁了，我在<span><%= school %></span>学习前端。</p>
+    </script>
+
+}
